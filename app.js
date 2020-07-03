25f7e183ec358729866644809844a2179c37e531
Bart <bart.rybak@hotmail.com>
2020-07-03 23:28:39 +0100

    delete item from array of input values

--- app.js
+++ app.js
@@ -63,6 +63,22 @@ var budgetController = (function() {
             return newItem;
         },
 
+        deleteItem: function(type, id) {
+            var ids, index;
+
+            // delete items from array
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+
+        },
+
         // calculate budget
         calculateBudget: function() {
 
@@ -253,9 +269,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             // split strings
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. delete item from data structure
+            budgetCtrl.deleteItem(type, ID);
 
             // 2. delete item from UI
 
